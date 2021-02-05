517b583a9f253a697df84bc5e6757b9910af8d76
tvrdoljak1 <tomislav-vrdoljak@outlook.com>
2021-02-05 14:25:45 +0100

    drugi commit

--- app/src/main/java/com/example/weatherapp2pokusaj/MainActivity.java
+++ app/src/main/java/com/example/weatherapp2pokusaj/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.weatherapp2pokusaj;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,9 +34,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import static java.lang.Math.round;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                 }
+             }
+    }
 
     EditText editText;
     Button button;
@@ -125,7 +157,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -138,5 +169,49 @@ public class MainActivity extends AppCompatActivity {
         });
         RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
         requestQueue.add(stringRequest);
+    }
+
+    //      LOCATION
+
+    public void getLocation(){
+        locationManager = (LocationManager) this.getSystemService((Context.LOCATION_SERVICE));
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                try {
+                    List<Address> listAddresses = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                    if(listAddresses != null && listAddresses.size() > 0){
+                        String address = "";
+
+                        if(listAddresses.get(0).getAdminArea() != null){
+                            address += listAddresses.get(0).getAdminArea() + " ";
+                        }
+
+                        if(listAddresses.get(0).getLocality() != null){
+                            address += listAddresses.get(0).getLocality() + " ";
+                        }
+
+                    Toast.makeText(getApplicationContext(),address,Toast.LENGTH_LONG).show();
+                        Log.i("INFO",address);
+
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    public void clickLocation(View v){
+        getLocation();
     }
 }
